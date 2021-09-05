228fe92a4ea968d445afd1ba8ebcec2441baf883
José Daniel Silva do Carmo <dan321daniel@gmail.com>
2021-09-05 11:37:33 -0300

    teste

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,7 +2,7 @@ import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
 import Login from "./pages/login";
 import Main from "./pages/main";
-
+//MESCLOU NELE
 export default createAppContainer(
     createSwitchNavigator({
         Login,
